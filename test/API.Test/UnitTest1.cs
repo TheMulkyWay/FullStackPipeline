ffa1c91e25d2483080f32fdf0a9301eddcfaf7fd
John-Mulkearns <manoflondon@hotmail.com>
2021-08-21 00:27:27 +0100

    Initial Commit

---
+++ test/API.Test/UnitTest1.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+
+
+
+
+namespace API.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+Assert.Equal("John Mulkearns", "John Mulkearns");
+// WeatherForecastController controller = new WeatherForecastController();
+        }
+    }
+
+
+public class xUnitTestPlay{
+
+        [Fact]
+        public void Test2()
+        {
+Assert.Equal("John J Mulkearns", "John J Mulkearns");
+// WeatherForecastController controller = new WeatherForecastController();
+        }
+}
+
+}
